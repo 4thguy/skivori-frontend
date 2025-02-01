a40839179907d5be55ee32caddc93dcfa1d25fcb
4thguy <ivanxuereb@gmail.com>
2025-02-01 11:21:19 +0100

    add navbar, home, and games pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { GamesComponent } from './pages/games/games.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    component: HomeComponent,
+  },
+  {
+    path: 'games',
+    title: 'Games',
+    component: GamesComponent,
+  },
+];
